e7a521e5f5790ac2dc12a12bfa609c1edc41498a
paschal533 <okwuosahpaschal@gmail.com>
2022-12-05 20:02:44 +0530

    first commit

--- styles/index.js
+++ styles/index.js
@@ -6,7 +6,7 @@ const styles = {
   yPaddings: 'sm:py-16 xs:py-8 py-12',
   xPaddings: 'sm:px-16 px-6',
   topPaddings: 'sm:pt-16 xs:pt-8 pt-12',
-  bottomPaddings: 'sm:pb-16 xs:pb-8 pb-12',
+  //bottomPaddings: 'sm:pb-16 xs:pb-8 pb-12',
 
   flexCenter: 'flex justify-center items-center',
   flexStart: 'flex justify-start items-start',
