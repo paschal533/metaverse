d57ada79c25889764a14a264213821fdc0c148ba
paschal533 <okwuosahpaschal@gmail.com>
2022-12-16 21:04:27 +0530

    update commit

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -2,7 +2,7 @@
 
 import { motion } from 'framer-motion';
 
-//import styles from '../styles';
+import styles from '../styles';
 import { fadeIn } from '../utils/motion';
 
 const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
